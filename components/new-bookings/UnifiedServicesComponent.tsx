ed6178731a8b352472ce219c19fb848ab4fca549
Raciel Ferrin <racielfg01@gmail.com>
2025-10-01 11:33:59 -0400

    fix(UnifiedS..):resolviendo el espacio para poder seleccionar el servicio

--- components/new-bookings/UnifiedServicesComponent.tsx
+++ components/new-bookings/UnifiedServicesComponent.tsx
@@ -434,7 +434,7 @@ export function UnifiedServicesComponent({
       )}
 
       {/* Grid de Servicios */}
-      <div className="grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-3">
+      <div className={`grid gap-2 lg:gap-4 grid-cols-2 lg:grid-cols-3 ${mode ==="selection"?"pb-44":""}`}>
         {filteredServices?.map((service) => {
           const isSelected =
             mode === "selection"
