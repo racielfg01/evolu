0f265ca84110d496ed3785af97467741c194c99a
Raciel Ferrin <racielfg01@gmail.com>
2026-06-10 17:58:53 -0400

    fix: actulizando los precios de en Usd

--- components/new-bookings/UnifiedServicesComponent.tsx
+++ components/new-bookings/UnifiedServicesComponent.tsx
@@ -79,7 +79,7 @@ export function UnifiedServicesComponent({
     useState<ServiceWithRelations | null>(null);
 
   useEffect(() => {
-    setUsdValue(600);
+    setUsdValue(650);
   }, []);
 
   // Efectos para calcular duración y precio total (solo en modo selección)
